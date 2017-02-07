3e495ba6cbf2349f3820eaf1b0ba55666899951d
Yaeri Lim <yaerilim@gmail.com>
2017-02-07 03:17:37 -0800

    added removeStudent

--- script.js
+++ script.js
@@ -7,16 +7,23 @@ function sgtReady(){
     console.log('SGT is ready');
     $(".btn-success").click(addClicked);
     $(".btn-default").click(cancelClicked);
-    if(student_array.length === 0){
-        $('.avgGrade').text(0);
-        $(".student-list-container").append("<h3><strong>User Info Unavailable</strong></h3>");
-    }
+    $('.avgGrade').text(0);
+    $(".student-list-container").append("<h3><strong>User Info Unavailable</strong></h3>");
+    reset();
 }
 
 function addClicked(){
     addStudent();
     clearAddStudentForm();
     updateData();
+}
+
+function cancelClicked(){
+    clearAddStudentForm();
+}
+
+function deleteClicked() {
+    removeStudent();
 }
 
 function addStudent(){
@@ -43,14 +50,11 @@ function addStudentToDom(studentObj){
     var nameTd = $("<td>").text(studentObj.name);
     var courseTd = $("<td>").text(studentObj.course);
     var gradeTd = $("<td>").text(studentObj.grade);
-    var deleteButton = $("<button>").html('Delete').addClass('btn btn-danger btn-xs')
+    var deleteButton = $("<button>").html('Delete').addClass('delete_button btn btn-danger btn-xs');
     var deleteTd = $("<td>").html(deleteButton);
     row.append(nameTd,courseTd,gradeTd,deleteTd);
     $("tbody").append(row);
-}
-
-function cancelClicked(){
-    clearAddStudentForm();
+    $(".delete_button").click(deleteClicked);
 }
 
 function clearAddStudentForm(){
@@ -77,4 +81,11 @@ function reset() {
     student_array = [];
     updateData();
     clearAddStudentForm();
+    $('.avgGrade').text(0);
+}
+function removeStudent() {
+    console.log('removeStudent');
+    var selectedRow = $(event.target).parent();
+    student_array.splice(selectedRow,1);
+    updateData();
 }
