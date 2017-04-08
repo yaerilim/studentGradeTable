a6916acb42ec332a2ceba0d3a2d24b6c25afdff2
Yaeri Lim <yaerilim@gmail.com>
2017-04-08 03:20:04 -0700

    added delete function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//---------------------------------- KEY PRESSED ----------------------------------
+var fbRef = firebase.database();
 $(document).ready(function() {
     $('body').keyup(function(event){               //clear input area when 'esc' key pressed
         if(event.which === 27){
@@ -10,6 +10,7 @@ $(document).ready(function() {
             add();
         }
     });
+    retrieveData();
 });
 //---------------------------------- CLEAR INPUT AREA ----------------------------------
 function cancel(){
@@ -18,24 +19,58 @@ function cancel(){
     $('#studentGrade').val('');
 }
 //---------------------------------- ADD INPUT VALUE TO TABLE ----------------------------------
+var studentArr = [];
+var studentObj = {};
 function add() {
-    var row = $('<tr>');
-    var name = $('<td>').append($('#studentName').val());
-    var course = $('<td>').append($('#course').val());
-    var grade = $('<td>').append($('#studentGrade').val());
-    var button = $('<td>').append($('<button>').html('Delete').addClass('btn btn-danger btn-md').click(operations));
-    row.append(name,course,grade,button);
-    $('tbody').append(row);
+    studentObj = {
+        name: $('#studentName').val(),
+        course: $('#course').val(),
+        grade : $('#studentGrade').val(),
+        key : null
+    };
+    studentArr.push(studentObj);
+    sendData(studentObj);
     cancel();
-    average();
+    calculateAverage();
 }
-//---------------------------------- DELETE THE SELECT ROW WHEN BUTTON CLICKED ----------------------------------
-function operations(){
-    $(this)[0].parentNode.parentNode.remove();
-    average();
+//---------------------------------- SEND DATA TO FIREBASE ----------------------------------
+function sendData(studentObj){
+    var dataToSend = {
+        name : studentObj.name,
+        course: studentObj.course,
+        grade : studentObj.grade,
+        key : null
+    };
+    fbRef.ref('students').push(dataToSend);
+    retrieveData();
 }
-//---------------------------------- SHOWING AVERAGE SCORE ----------------------------------
-function average(){
+//---------------------------------- RETRIEVE DATA FROM FIREBASE ----------------------------------
+function retrieveData(){
+    fbRef.ref('students').on('value', function (snapshot) {
+        addDataToDom(snapshot.val());
+    });
+}
+//---------------------------------- POST FIREBASE DATA TO THE TABLE ----------------------------------
+function addDataToDom(firebaseObj){
+    $('tbody').empty();
+    var x;
+    var key = Object.keys(firebaseObj);
+    for(x in firebaseObj){
+        var row = $('<tr>');
+        var nameTd = $('<td>').append(firebaseObj[x].name);
+        var courseTd = $('<td>').append(firebaseObj[x].course);
+        var gradeTd = $('<td>').append(firebaseObj[x].grade);
+        var editBtn = $('<button>').html('Edit').addClass('btn btn-primary btn-sm').click(editStudent).css("margin-right", "3%");
+        var deleteBtn = $('<button>').html('Delete').addClass('btn btn-danger btn-sm').click(deleteStudent).val(key[0]);
+        key.splice(0,1);
+        var buttons = $('<td>').append(editBtn, deleteBtn);
+        row.append(nameTd,courseTd,gradeTd,buttons);
+        $('tbody').append(row);
+    }
+    calculateAverage();
+}
+//---------------------------------- CALCULATE AVERAGE GRADE ----------------------------------
+function calculateAverage(){
     var grade = 0;
     for(var i = 0; i<$('tbody')[0].childElementCount; i++){
         var eachGrade = parseInt($('tbody')[0].children[i].children[2].innerText);
@@ -48,3 +83,15 @@ function average(){
         $('.avgGrade').text(averageGrade);
     }
 }
+//---------------------------------- DELETE THE SELECT ROW WHEN BUTTON CLICKED ----------------------------------
+function deleteStudent(){
+    fbRef.ref('students/' + $(this).val()).remove();
+    calculateAverage();
+}
+//---------------------------------- EDIT THE INFORMATION ON SELECT ROW WHEN BUTTON CLICKED ----------------------------------
+function editStudent(){
+    console.log('edit');
+    calculateAverage();
+}
+
+
